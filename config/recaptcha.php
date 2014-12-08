d37adc3e922bf701a0414e752b81dd3e994737e8
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-12-08 23:56:08 +0800

    initial recaptcha library.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ config/recaptcha.php
@@ -0,0 +1,13 @@
+<?php
+
+// To use reCAPTCHA, you need to sign up for an API key pair for your site.
+// link: http://www.google.com/recaptcha/admin
+$config['recaptcha_site_key'] = '';
+$config['recaptcha_secret_key'] = '';
+
+// reCAPTCHA supported 40+ languages listed here:
+// https://developers.google.com/recaptcha/docs/language
+$config['recaptcha_lang'] = 'en';
+
+/* End of file recaptcha.php */
+/* Location: ./application/config/recaptcha.php */
---
+++ libraries/recaptcha.php
@@ -0,0 +1,103 @@
+<?php (! defined('BASEPATH')) and exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Recaptcha library
+ *
+ * @package CodeIgniter
+ * @author  Bo-Yi Wu <appleboy.tw@gmail.com>
+ * @link    https://github.com/appleboy/CodeIgniter-reCAPTCHA
+ */
+class recaptcha
+{
+    /**
+     * ci instance object
+     *
+     */
+    private $_ci;
+
+    /**
+     * reCAPTCHA site up and verify url.
+     *
+     */
+    const sign_up_url = 'https://www.google.com/recaptcha/admin';
+    const site_verify_url = 'https://www.google.com/recaptcha/api/siteverify';
+
+    /**
+     * constructor
+     *
+     * @param string $config
+     */
+    public function __construct()
+    {
+        $this->_ci = & get_instance();
+        $this->_ci->load->config('recaptcha');
+        $this->_siteKey = $this->_ci->config->item('recaptcha_site_key');
+        $this->_secretKey = $this->_ci->config->item('recaptcha_secret_key');
+
+        if (empty($this->_siteKey) or empty($this->_secretKey)) {
+            die("To use reCAPTCHA you must get an API key from <a href='"
+                .self::sign_up_url."'>".self::sign_up_url."</a>");
+        }
+    }
+
+    /**
+     * Submits an HTTP GET to a reCAPTCHA server.
+     *
+     * @param array $data array of parameters to be sent.
+     *
+     * @return array response
+     */
+    private function _submitHTTPGet($data)
+    {
+        $url = self::site_verify_url.'?'.http_build_query($data);
+        $response = file_get_contents($url);
+
+        return $response;
+    }
+
+    /**
+     * Calls the reCAPTCHA siteverify API to verify whether the user passes
+     * CAPTCHA test.
+     *
+     * @param string $response response string from recaptcha verification.
+     * @param string $remoteIp IP address of end user.
+     *
+     * @return ReCaptchaResponse
+     */
+    public function verifyResponse($response, $remoteIp = null)
+    {
+        $remoteIp = (!empty($remoteIp)) ? $remoteIp : $this->_ci->input->ip_address();
+
+        // Discard empty solution submissions
+        if (empty($response)) {
+            return array(
+                'success' => false,
+                'error-codes' => 'missing-input',
+            );
+        }
+
+        $getResponse = $this->_submitHttpGet(
+            array(
+                'secret' => $this->_secretKey,
+                'remoteip' => $remoteIp,
+                'response' => $response,
+            )
+        );
+
+        // get reCAPTCHA server response
+        $responses = json_decode($getResponse, true);
+
+        if (isset($responses['success']) and $responses['success'] == true) {
+            $status = true;
+        } else {
+            $status = false;
+            $error = (isset($responses['error-codes'])) ? $responses['error-codes']
+                : 'invalid-input-response';
+        }
+
+        return array(
+            'success' => $status,
+            'error-codes' => (isset($error)) ? $error : null,
+        );
+    }
+}
